4295c2682019cfd1afaded190429c8ca8eb1797b
PhyoPhyoMa <phyophyoma18@gmail.com>
2018-09-03 22:54:51 +0630

    font converter

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-from flask import Flask, render_template, jsonify, request
-import controller
-import pyperclip
+from flask import Flask, render_template, url_for, request, jsonify
+import zg2uni
+import uni2zg
+import win2uni
+import uni2win
 
-app = Flask(__name__)
+myapp=Flask(__name__)
 
+@myapp.route("/")
+def hello():
+    return render_template("index.html")
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/_convert', methods=["POST"])
+@myapp.route("/convert", methods=["POST"])
 def convert():
-    from_text = request.form['from_text']
-    from_encoding = request.form['from_encoding']
-    to_encoding = request.form['to_encoding']
-    res = controller.convert(from_encoding, to_encoding, from_text)
-    return jsonify(result=res)
-
+    myinput = request.form['myinput']
+    myoutput = request.form['myoutput']
+    source = request.form['source']
+    output = "The Input is " + myinput +"\r\n" + "The Output is " +myoutput
+    if myinput == "zawgyi" and myoutput == "unicode":
+        output = zg2uni.convert(source)
+    if myinput == "zawgyi" and myoutput == "winmyanmar":
+        output = zg2uni.convert(uni2win.convert(source))
+    if myinput == "unicode" and myoutput == "zawgyi":
+        output = uni2zg.convert(source)
+    if myinput == "unicode" and myoutput == "winmyanmar":
+        output = uni2win.convert(source)
+    if myinput == "winmyanmar" and myoutput == "zawgyi":
+        output = uni2zg.convert(win2uni.convert(source))
+    if myinput == "winmyanmar" and myoutput == "unicode":
+        output = win2uni.convert(source)
+    if myinput == myoutput:
+        output = "Input is: " + myinput + "and Output is: " + \
+                myoutput + ". There are both same.No conversion done!"
+    return jsonify ({'output': output})
 
 if __name__ == "__main__":
-app.run(debug=True)
+    myapp.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,26 @@
-$(document).ready(function () {
-    $('#convert').bind('click', function (event) {
-        $.ajax({
-            type: "POST",
-            url: "/_convert",
-            data: {
-                from_text: $('textarea[name="from_text"]').val(),
-                to_encoding: $('input[name="to"]:checked').val(),
-                from_encoding: $("input[name='from']:checked").val()
-            }
-        }).done(function (data) {
-            if (data.error) {
-                //  show error message
-            } else {
-                $("#to_text").val(data.result).show();
-                $("#to_text_hint").hide();
-            }
-        });
-        event.preventDefault();
-    });
-
-    $('input[type=radio][name=from]').change(function () {
-        if (this.value === 'from_zawgyi') {
-            $("#from_text").css("font-family", "Zawgyi-One");
-        } else if (this.value === 'from_unicode') {
-            $("#from_text").css("font-family", "Padauk");
-        } else if (this.value === 'from_win') {
-            $("#from_text").css("font-family", "WinInnwa");
-        }
-    });
-    $('input[type=radio][name=to]').change(function () {
-        if (this.value === 'to_zawgyi') {
-            $("#to_text").css("font-family", "Zawgyi-One");
-        } else if (this.value === 'to_unicode') {
-            $("#to_text").css("font-family", "Padauk");
-        } else if (this.value === 'to_win') {
-            $("#to_text").css("font-family", "WinInnwa");
-        }
-    });
+$(document).ready(function(){
+	$('form').on('submit', function(event){
+		$.ajax({
+			type: "POST",
+			url: "/convert",
+			success: function(response){
+				
+			},
+			data: {
+				myinput: $('input[name=input]:checked').val(),
+				myoutput: $('input[name=output]:checked').val(),
+				source: $('#source').val()
+			}
+		})
+		.done(function(data){
+			if(data.error){
+				// Do something
+			}
+			else{
+				
+				$("#destination").val(data.output).show();
+			}
+		});
+		event.preventDefault();
+	});
 });
-
